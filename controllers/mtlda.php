1e8635c5d3d4dfed9ee44204501e0a7bc5328f08
Andreas Unterkircher <unki@netshadow.at>
2015-06-27 11:18:45 +0200

    call HTTP-Router controller

--- controllers/mtlda.php
+++ controllers/mtlda.php
@@ -2,17 +2,25 @@
 
 require_once BASE_PATH."/controllers/config.php";
 require_once BASE_PATH."/controllers/db.php";
+require_once BASE_PATH."/controllers/http_router.php";
 
 
 class MTLDA_Controller {
 
     public function __construct()
     {
-        global $config_ctrl, $db;
+        global $db, $query;
 
-        $config_ctrl = new MTLDA_Config_Controller;
-
+        $cfg = new MTLDA_Config_Controller;
         $db = new MTLDA_DB_Controller;
+
+        if(!isset($_SERVER['REQUEST_URI']) || empty($_SERVER['REQUEST_URI'])) {
+            print "Error - \$_SERVER['REQUEST_URI'] is not set!";
+            exit(1);
+        }
+
+        $router = new MTLDA_HTTP_Router_Controller;
+        $query = $router->parse($_SERVER['REQUEST_URI']);
     }
 }
 
