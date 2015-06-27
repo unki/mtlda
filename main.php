ff08ac55b18565e184682157984c4dfc746a45ca
Andreas Unterkircher <unki@netshadow.at>
2015-06-27 08:26:20 +0200

    add ../main.php

---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASE_PATH', __DIR__);
+require_once BASE_PATH.'/controllers/mtlda.php';
+
+$mtlda = new MTLDA_Controller;
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
