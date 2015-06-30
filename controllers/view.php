088b2d3ac837262f26246961bef57f7dca93043c
Andreas Unterkircher <unki@netshadow.at>
2015-06-30 08:09:39 +0200

    use namespace in ViewController

--- controllers/view.php
+++ controllers/view.php
@@ -1,29 +1,38 @@
 <?php
 
-class MTLDA_View_Controller {
+namespace MTLDA\Controllers;
 
-    var $page_map = Array(
-        '/^$/' => 'View_Controller_Index',
-        '/^index$/' => 'View_Controller_Index',
-        '/^queue$/' => 'View_Controller_Queue',
+use MTLDA\Views;
+
+class ViewsController
+{
+    private $page_map = array(
+        '/^$/' => 'IndexView',
+        '/^index$/' => 'IndexView',
+        '/^queue$/' => 'QueueView',
     );
 
-    public function get_controller_name($view)
+    public function getViewName($view)
     {
-        foreach(array_keys($this->page_map) as $entry) {
+        foreach (array_keys($this->page_map) as $entry) {
 
-            if(($result = preg_match($entry, $view)) === FALSE) {
+            if (($result = preg_match($entry, $view)) === false) {
                 print "Error - unable to match ${entry} in ${view}";
                 exit(1);
             }
 
-            if($result == 0)
+            if ($result == 0) {
                 continue;
+            }
+
+            if (!class_exists($this->page_map[$entry])) {
+                print "Error - view class ". $this->page_map[$entry] ." does not exist!";
+                exit(1);
+            }
 
             return $this->page_map[$entry];
 
         }
-
     }
 }
 
