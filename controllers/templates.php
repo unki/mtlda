5f710dffdb9802b7a52d80cc728979e0de27c11e
Andreas Unterkircher <unki@netshadow.at>
2015-06-30 07:41:56 +0200

    add namespace to TemplatesController

---
+++ controllers/templates.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MTLDA\Controllers ;
+
+class TemplatesController extends Smarty
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->template_dir = BASE_PATH .'/cache/templates';
+        $this->compile_dir  = BASE_PATH .'/cache/templates_c';
+        $this->config_dir   = BASE_PATH .'/cache/smarty_config';
+        $this->cache_dir    = BASE_PATH .'/cache/smarty_cache';
+
+        if (!is_writeable($this->compile_dir)) {
+            print "Error - Smarty compile directory ". $this->compile_dir ." is not writeable
+                for the current user (". $ms->getuid() .").<br />\n";
+            print "Please check that permissions are set correctly to this directory.<br />\n";
+            exit(1);
+        }
+    }
+}
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
