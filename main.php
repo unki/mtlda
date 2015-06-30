9ee6a8e3d6edc8c56c1d856eb09c2fa31729a5d4
Andreas Unterkircher <unki@netshadow.at>
2015-06-30 08:04:10 +0200

    main.php - change to namespace usage

--- main.php
+++ main.php
@@ -1,8 +1,10 @@
 <?php
 
-define('BASE_PATH', __DIR__);
+require_once "constant.php";
 require_once BASE_PATH.'/controllers/mtlda.php';
 
-$mtlda = new MTLDA_Controller;
+use MTLDA\Controllers as Controllers;
+
+$mtlda = new Controllers\MTLDAController;
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
