c6415f5d642ad2c7d96e126b245884e583be523d
Andreas Unterkircher <unki@netshadow.at>
2015-06-30 09:14:46 +0200

    templates.php - get current user id

--- views/templates.php
+++ views/templates.php
@@ -4,7 +4,7 @@ namespace MTLDA\Views ;
 
 use Smarty;
 
-class ViewTemplate extends Smarty
+class Templates extends Smarty
 {
     public function __construct()
     {
@@ -17,11 +17,23 @@ class ViewTemplate extends Smarty
 
         if (!is_writeable($this->compile_dir)) {
             print "Error - Smarty compile directory ". $this->compile_dir ." is not writeable
-                for the current user (". $ms->getuid() .").<br />\n";
+                for the current user (". $this->getuid() .").<br />\n";
             print "Please check that permissions are set correctly to this directory.<br />\n";
             exit(1);
         }
     }
+
+    public function getuid()
+    {
+        if ($uid = posix_getuid()) {
+            if ($user = posix_getpwuid($uid)) {
+                return $user['name'];
+            }
+        }
+
+        return 'n/a';
+
+    } // getuid()
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
