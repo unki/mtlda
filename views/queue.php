be8de2d12e1b2209c0f81e457be019a5248334ec
Andreas Unterkircher <unki@netshadow.at>
2015-07-06 06:59:52 +0200

    QueueView - add edit mode

--- views/queue.php
+++ views/queue.php
@@ -7,6 +7,7 @@ use MTLDA\Models;
 class QueueView extends Templates
 {
     public $class_name = 'queue';
+    public $item_name = 'QueueItem';
     public $queue;
 
     public function __construct()
@@ -16,6 +17,13 @@ class QueueView extends Templates
         parent::__construct();
 
         print $this->show();
+    }
+
+    public function showEdit($id, $hash)
+    {
+        $item = new Models\QueueItemModel($id, $hash);
+
+        parent::showEdit($item);
     }
 
     public function queueList($params, $content, &$smarty, &$repeat)
@@ -37,6 +45,7 @@ class QueueView extends Templates
         $item =  $this->queue->items[$item_idx];
 
         $smarty->assign("item", $item);
+        $smarty->assign("item_safe_link", $item->queue_idx ."+". $item->queue_file_hash);
 
         $index++;
         $smarty->assign('smarty.IB.item_list.index', $index);
