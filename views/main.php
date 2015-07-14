bd4d48b461b1c6e63d66fcd96d2a58efcfefff33
Andreas Unterkircher <unki@netshadow.at>
2015-07-14 07:46:45 +0200

    MainView - remove unneeded code lines

--- views/main.php
+++ views/main.php
@@ -5,18 +5,7 @@ namespace MTLDA\Views;
 class MainView extends Templates
 {
     public $class_name = 'main';
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        print $this->fetch('main.tpl');
-    }
-
-    public function mainList()
-    {
-
-    }
+    public $default_mode = 'show';
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
