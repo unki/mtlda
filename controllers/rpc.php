4553ae31eae7bb6d81b47a13350753853d659190
Andreas Unterkircher <unki@netshadow.at>
2015-07-18 07:39:00 +0200

    RpcController - add archive action, refs #475

--- controllers/rpc.php
+++ controllers/rpc.php
@@ -23,6 +23,9 @@ class RpcController
             case 'delete':
                 $this->rpcDeleteObject();
                 break;
+            case 'archive':
+                $this->rpcArchiveObject();
+                break;
             case 'toggle':
                 $this->rpc_toggle_object_status();
                 break;
@@ -32,30 +35,18 @@ class RpcController
             case 'alter-position':
                 $this->rpc_alter_position();
                 break;
-            case 'graph-data':
-                $this->rpc_graph_data();
-                break;
-            case 'graph-mode':
-                $this->rpc_graph_mode();
-                break;
             case 'get-content':
                 $this->rpc_get_content();
                 break;
             case 'get-sub-menu':
                 $this->rpc_get_sub_menu();
                 break;
-            case 'set-host-profile':
-                $this->rpc_set_host_profile();
-                break;
-            case 'get-host-state':
-                $this->rpc_get_host_state();
-                break;
             case 'idle':
                 // just do nothing, for debugging
                 print "ok";
                 break;
             default:
-                print "Unknown action\n";
+                print "Unknown RPC action\n";
                 return false;
                 break;
         }
@@ -111,7 +102,7 @@ class RpcController
             print "id contains an invalid guid!";
             return false;
         }
-            
+
         $request_object = $parts[1];
         $id = $parts[2];
         $guid = $parts[3];
@@ -129,6 +120,84 @@ class RpcController
         print "unknown error";
         return false;
 
+    }
+
+    private function rpcArchiveObject()
+    {
+        global $mtlda, $query;
+
+        if (!isset($_POST['id'])) {
+            print "id is missing!";
+            return false;
+        }
+
+        if (!$mtlda->isValidId($_POST['id'])) {
+            print "id looks invalid!";
+            return false;
+        }
+
+        $id = $_POST['id'];
+
+        $parts = array();
+        if (preg_match('/(\w+)-([0-9]+)\+([a-z0-9]+)/', $id, $parts) === false) {
+            print "id in incorrect format!";
+            return false;
+        }
+
+        /* $parts() should now contain
+         * [0] = original id
+         * [1] = object (queueitem, etc.)
+         * [2] = queue_idx
+         * [3] = guid
+         */
+        if (!array($parts) || empty($parts) || count($parts) != 4) {
+            print "id does not contain all required information!";
+            return false;
+        }
+
+        if (!isset($parts[1]) || !$mtlda->isValidModel($parts[1])) {
+            print "id contains an invalid model!";
+            return false;
+        }
+
+        if (!isset($parts[2]) || !is_numeric($parts[2])) {
+            print "id contains an invalid idx!";
+            return false;
+        }
+
+        if (!isset($parts[3]) || !$mtlda->isValidGuidSyntax($parts[3])) {
+            print "id contains an invalid guid!";
+            return false;
+        }
+
+        $request_object = $parts[1];
+        $id = $parts[2];
+        $guid = $parts[3];
+
+        if ($request_object != "queueitem") {
+            print "archive function can only be used for Queue items!";
+            return false;
+        }
+
+        if (!($obj = $this->loadModel($request_object, $id, $guid))) {
+            print "unable to locate model for ${request_object}!";
+            return false;
+        }
+
+        $storage = new StorageController;
+
+        if (!$storage) {
+            print "unable to load StorageController!";
+            return false;
+        }
+
+        if (!$storage->archive($obj)) {
+            print "StorageController::archive() exited with an error!";
+            return false;
+        }
+
+        print "ok";
+        return true;
     }
 
     public function loadModel($object_name, $id = null, $guid = null)
