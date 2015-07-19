0ab0d39df53610e5679b3b334c4d5098b6af557d
Andreas Unterkircher <unki@netshadow.at>
2015-07-19 11:58:20 +0200

    add PreviewView

--- views/preview.php
+++ views/preview.php
@@ -7,9 +7,30 @@ class PreviewView extends Templates
     public $class_name = 'preview';
     public $default_mode = 'show';
 
-    public function __construct()
+    public function show()
     {
-        parent::__construct();
+        global $mtlda, $query, $config;
+
+        if (!isset($query) || !isset($query->params) || empty($query->params)) {
+            $mtlda->raiseError("\$query->params not set!");
+            return false;
+        }
+
+        if (!isset($query->params['id']) || empty($query->params['id'])) {
+            $mtlda->raiseError("\$query->id not set or empty!");
+            return false;
+        }
+
+        if (!$mtlda->isValidId($query->params['id'])) {
+            $mtlda->raiseError("\$query->id has an invalid syntax!");
+            return false;
+        }
+
+        $img_url = $config['app']['base_web_path'] .'/preview/'. $query->params['id'];
+
+        $this->assign('img_url', $img_url);
+
+        return $this->fetch("preview.tpl");
     }
 }
 
