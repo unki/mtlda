aafaa0ab2e6273ff99ab2dd99ad4368e8c45be66
Andreas Unterkircher <unki@netshadow.at>
2015-07-19 11:47:40 +0200

    PreviewView - add constructor

---
+++ views/preview.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MTLDA\Views;
+
+class PreviewView extends Templates
+{
+    public $class_name = 'preview';
+    public $default_mode = 'show';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
