7f265dde6e68dc37dc1278552f1566508e772b49
Andreas Unterkircher <unki@netshadow.at>
2015-07-23 05:59:12 +0200

    add GNU Affero General Public License to all source code files

--- incoming_watcher.sh
+++ incoming_watcher.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# This file is part of MTLDA.
+#
+# MTLDA, a web-based document archive.
+# Copyright (C) <2015>  <Andreas Unterkircher>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
 MTDLA_PATH=/home/unki/git/mtlda
 INCOM=/usr/bin/inoticoming
 INPATH=${MTDLA_PATH}/data/incoming
