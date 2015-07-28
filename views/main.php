bc54858c622f7ff56342efbeea179573993bebd4
Andreas Unterkircher <unki@netshadow.at>
2015-07-28 06:57:33 +0200

    MainView, remove  attribute

--- views/main.php
+++ views/main.php
@@ -22,7 +22,6 @@ namespace MTLDA\Views;
 class MainView extends Templates
 {
     public $class_name = 'main';
-    public $default_mode = 'show';
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
