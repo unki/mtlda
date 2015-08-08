8299203251635c4f4284e09ecac45bb692f4b0e0
Andreas Unterkircher <unki@netshadow.at>
2015-08-08 20:51:48 +0200

    add DefaultController, refs #484

---
+++ controllers/default.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of MTLDA.
+ *
+ * MTLDA, a web-based document archive.
+ * Copyright (C) <2015>  <Andreas Unterkircher>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
+namespace MTLDA\Controllers;
+
+class DefaultController
+{
+    const CONFIG_DIRECTORY = MTLDA_BASE ."/config";
+    const CACHE_DIRECTORY = MTLDA_BASE ."/cache";
+    const ARCHIVE_DIRECTORY = MTLDA_BASE."/data/archive";
+    const INCOMING_DIRECTORY = MTLDA_BASE ."/data/incoming";
+    const WORKING_DIRECTORY = MTLDA_BASE ."/data/working";
+}
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
