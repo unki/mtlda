d22084417bec9ee9783b410456c2ef177c451348
Andreas Unterkircher <unki@netshadow.at>
2015-08-08 11:03:38 +0200

    further work on InstallerController, #485

--- controllers/installer.php
+++ controllers/installer.php
@@ -25,7 +25,7 @@ class InstallerController
 
     public function setup()
     {
-        global $db, $mtlda;
+        global $mtlda, $db, $config;
 
         if ($db->checkTableExists("TABLEPREFIXmeta")) {
             if (($this->schema_version_before = $db->getDatabaseSchemaVersion()) === false) {
@@ -38,8 +38,8 @@ class InstallerController
 
         if ($this->schema_version_before < $db->getSoftwareSchemaVersion()) {
 
-            if (!$this->installDatabaseTables()) {
-                $mtlda->raiseError("InstallerController::installTables() returned false!");
+            if (!$this->createDatabaseTables()) {
+                $mtlda->raiseError("InstallerController::createDatabaseTables() returned false!");
                 return false;
             }
         }
@@ -50,10 +50,138 @@ class InstallerController
         }
 
         if (!empty($this->schema_version_before)) {
-            $mtlda->write("Database schema version before upgrade: {$this->schema_version_before}");
+            print "Database schema version before upgrade: {$this->schema_version_before}<br />\n";
         }
-        $mtlda->write("Software supported schema version: {$db->getSoftwareSchemaVersion()}");
-        $mtlda->write("Database schema version after upgrade: {$db->getDatabaseSchemaVersion()}");
+        print "Software supported schema version: {$db->getSoftwareSchemaVersion()}<br />\n";
+        print "Database schema version after upgrade: {$db->getDatabaseSchemaVersion()}<br />\n";
+
+        print "<a href='{$config->getWebPath()}'>Return to application</a><br />\n";
+
+        return true;
+    }
+
+    private function createDatabaseTables()
+    {
+        global $mtlda, $db;
+
+        if (!$db->checkTableExists("TABLEPREFIXarchive")) {
+
+            $table_sql = "CREATE TABLE `TABLEPREFIXarchive` (
+                `document_idx` int(11) NOT NULL AUTO_INCREMENT,
+                `document_guid` varchar(255) DEFAULT NULL,
+                `document_file_name` varchar(255) DEFAULT NULL,
+                `document_file_hash` varchar(255) DEFAULT NULL,
+                `document_file_size` int(11) DEFAULT NULL,
+                `document_signing_icon_position` int(11) DEFAULT NULL,
+                `document_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+                `document_version` varchar(255) DEFAULT NULL,
+                `document_derivation` int(11) DEFAULT NULL,
+                `document_derivation_guid` varchar(255) DEFAULT NULL,
+                PRIMARY KEY (`document_idx`)
+                    )
+                    ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+            if ($db->query($table_sql) === false) {
+                $mtlda->raiseError("Failed to create 'archive' table");
+                return false;
+            }
+        }
+
+        if (!$db->checkTableExists("TABLEPREFIXaudit")) {
+
+            $table_sql = "CREATE TABLE `TABLEPREFIXaudit` (
+                `audit_idx` int(11) NOT NULL AUTO_INCREMENT,
+                `audit_guid` varchar(255) DEFAULT NULL,
+                `audit_type` varchar(255) DEFAULT NULL,
+                `audit_scene` varchar(255) DEFAULT NULL,
+                `audit_message` text,
+                `audit_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+                PRIMARY KEY (`audit_idx`)
+                    )
+                    ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+            if ($db->query($table_sql) === false) {
+                $mtlda->raiseError("Failed to create 'audit' table");
+                return false;
+            }
+        }
+
+        if (!$db->checkTableExists("TABLEPREFIXqueue")) {
+
+            $table_sql = "CREATE TABLE `TABLEPREFIXqueue` (
+                `queue_idx` int(11) NOT NULL AUTO_INCREMENT,
+                `queue_guid` varchar(255) DEFAULT NULL,
+                `queue_file_name` varchar(255) DEFAULT NULL,
+                `queue_file_hash` varchar(255) DEFAULT NULL,
+                `queue_file_size` int(11) DEFAULT NULL,
+                `queue_signing_icon_position` int(11) DEFAULT NULL,
+                `queue_state` varchar(255) DEFAULT NULL,
+                `queue_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+                PRIMARY KEY (`queue_idx`)
+                    )
+                    ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+            if ($db->query($table_sql) === false) {
+                $mtlda->raiseError("Failed to create 'queue' table");
+                return false;
+            }
+        }
+
+        if (!$db->checkTableExists("TABLEPREFIXmeta")) {
+
+            $table_sql = "CREATE TABLE `TABLEPREFIXmeta` (
+                `meta_idx` int(11) NOT NULL auto_increment,
+                `meta_key` varchar(255) default NULL,
+                `meta_value` varchar(255) default NULL,
+                PRIMARY KEY  (`meta_idx`),
+                UNIQUE KEY `meta_key` (`meta_key`)
+                    )
+                    ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+
+            if ($db->query($table_sql) === false) {
+                $mtlda->raiseError("Failed to create 'meta' table");
+                return false;
+            }
+
+            if (!$this->setDatabaseSchemaVersion()) {
+                $mtlda->raiseError("Failed to set schema verison!");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function setDatabaseSchemaVersion($version = null)
+    {
+        global $mtlda, $db;
+
+        if (!isset($version) || empty($version)) {
+            $version = $db->getSoftwareSchemaVersion();
+        }
+
+        $result = $db->query(
+            "REPLACE INTO TABLEPREFIXmeta (
+            meta_key,
+            meta_value
+                ) VALUES (
+                    'schema_version',
+                    '{$version}'
+                    )"
+        );
+
+        if ($result === false) {
+            $mtlda->raiseError("Unable to set schema_version in meta table!");
+            return false;
+        }
+
+        return true;
+
+    }
+
+    private function upgradeDatabaseSchema()
+    {
+
 
         return true;
     }
