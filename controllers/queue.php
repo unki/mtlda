62f5ef280f3a46726032f54fb2a50738a36048f1
Andreas Unterkircher <unki@netshadow.at>
2015-08-29 16:52:57 +0200

    add QueueController, refs #492

---
+++ controllers/queue.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This file is part of MTLDA.
+ *
+ * MTLDA, a web-based document archive.
+ * Copyright (C) <2015>  <Andreas Unterkircher>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
+namespace MTLDA\Controllers;
+
+use MTLDA\Models;
+
+class QueueController extends DefaultController
+{
+    public function archive($id, $guid)
+    {
+        global $mtlda;
+
+        if (empty($id) || !is_numeric($id)) {
+            $mtlda->raiseError("id is invalid!");
+            return false;
+        }
+
+        if (empty($guid) || !$mtlda->isValidGuidSyntax($guid)) {
+            $mtlda->raiseError("guid is invalid!");
+            return false;
+        }
+
+        if (!($obj = $mtlda->loadModel("queueitem", $id, $guid))) {
+            $mtlda->raiseError("Unable to load model for {$id}, {$guid}");
+            return false;
+        }
+
+        try {
+            $storage = new StorageController;
+        } catch (Exception $e) {
+            $mtlda->raiseError("Failed to load StorageController!");
+            return false;
+        }
+
+        if (!$storage) {
+            $mtlda->raiseError("Unable to load StorageController!");
+            return false;
+        }
+
+        if (!$storage->archive($obj)) {
+            $mtlda->raiseError("StorageController::archive() exited with an error!");
+            return false;
+        }
+
+        return true;
+    }
+
+    public function archiveAll()
+    {
+        global $mtlda;
+
+        try {
+            $queue = new Models\QueueModel;
+        } catch (Exception $e) {
+            $mtlda->raiseError("Failed to load QueueModel");
+            return false;
+        }
+
+        foreach ($queue->avail_items as $key) {
+            $queueitem = $queue->items[$key];
+            $idx = $queueitem->queue_idx;
+            $guid = $queueitem->queue_guid;
+            if (empty($idx) || !$mtlda->isValidGuidSyntax($guid)) {
+                continue;
+            }
+            if (!$this->archive($idx, $guid)) {
+                $mtlda->raiseArchive(__CLASS__ ."::archive() returned false for QueueItem {$idx}, {$guid}!");
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
