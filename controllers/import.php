000edcbbfd43ca12db45b71e30c14d7afe055365
Andreas Unterkircher <unki@netshadow.at>
2015-09-25 14:31:00 +0200

    ImportController - scan incoming directory also for subdirectories, refs #502

--- controllers/import.php
+++ controllers/import.php
@@ -38,73 +38,37 @@ class ImportController extends DefaultController
     {
         global $mtlda, $config, $audit;
 
-        if (( $importdir = opendir($this::INCOMING_DIRECTORY)) === false) {
-            print "Error!: failed to access ". $this::INCOMING_DIRECTORY;
-            die();
-        }
-
         if ($config->isCreatePreviewImageOnImport()) {
-
             try {
                 $imagectrl = new Controllers\ImageController;
-            } catch (Exception $e) {
+            } catch (\Exception $e) {
                 $mtlda->raiseError("Unable to load ImageController");
                 return false;
             }
         }
 
-        while ($file = readdir($importdir)) {
+        if (
+            $config->isPdfSigningEnabled() &&
+            (($sign_pos = $config->getPdfSigningIconPosition()) === false)
+        ) {
+            $mtlda->raiseError("PDF-Signing is enabled but no signing-icon-position found!");
+            return false;
+        }
 
-            $in_file = $this::INCOMING_DIRECTORY ."/". $file;
-            $work_file = $this::WORKING_DIRECTORY ."/". $file;
+        $files = array();
 
-            // ignore special files and if $file is empty
-            if (empty($file) || $file == '.' || $file == '..') {
-                continue;
-            }
+        if (!$this->scanDirectory($this::INCOMING_DIRECTORY, $files)) {
+            $mtlda->raiseError(__CLASS__ .'::scanDirectory() returned false!');
+            return false;
+        }
 
-            // ignore file/directories starting with .
-            if (preg_match("/^\..*/", $file)) {
-                continue;
-            }
+        if (!isset($files) || empty($files)) {
+            return true;
+        }
 
-            if (!file_exists($in_file)) {
-                $mtdla->raiseError(__TRAIT__ ." file {$in_file} does not exist!");
-                return false;
-            }
+        foreach ($files as $file) {
 
-            if (!is_file($in_file)) {
-                $mtlda->raiseError(__TRAIT__ ." {$in_file} is no file!");
-                return false;
-            }
-
-            if (!is_readable($in_file)) {
-                $mtdla->raiseError(__TRAIT__ ." {$in_file} is not readable!");
-                return false;
-            }
-
-            try {
-                $audit->log(
-                    "{$in_file}",
-                    "new",
-                    "queue"
-                );
-            } catch (Exception $e) {
-                $mtlda->raiseError("AuditController::log() raised an exception!");
-                return false;
-            }
-
-            if (($hash = sha1_file($in_file)) === false) {
-                $mtdla->raiseError(__TRAIT__ ." SHA1 value of {$in_file} can not be calculated!");
-                return false;
-            }
-
-            if (($size = filesize($in_file)) === false) {
-                $mtdla->raiseError(__TRAIT__ ." filesize of {$in_file} is not available!");
-                return false;
-            }
-
-            if (!($guid = $mtlda->createGuid())) {
+            if (!($file['guid'] = $mtlda->createGuid())) {
                 $mtdla->raiseError(__TRAIT__ ." no valid GUID returned by createGuid()!");
                 return false;
             }
@@ -116,41 +80,38 @@ class ImportController extends DefaultController
                 return false;
             }
 
-            $queueitem->queue_guid = $guid;
-            $queueitem->queue_file_name = $file;
-            $queueitem->queue_file_size = $size;
-            $queueitem->queue_file_hash = $hash;
+            $queueitem->queue_guid = $file['guid'];
+            $queueitem->queue_file_name = $file['filename'];
+            $queueitem->queue_file_size = $file['size'];
+            $queueitem->queue_file_hash = $file['hash'];
             $queueitem->queue_state = 'new';
             $queueitem->queue_time = microtime(true);
 
             if ($config->isPdfSigningEnabled()) {
-
-                if ($pos = $config->getPdfSigningIconPosition()) {
-                    $queueitem->queue_signing_icon_position = $pos;
-                } else {
-                    $mtlda->raiseError(__TRAIT__ ." PDF-Signing is enabled but no signing-icon-position found!");
-                    return false;
-                }
+                $queueitem->queue_signing_icon_position = $sign_pos;
             }
 
             if (!$queueitem->save()) {
                 $queueitem->delete();
-                $mtlda->raiseError(__TRAIT__ ." saving QueueItemModel failed!");
+                $mtlda->raiseError("Saving QueueItemModel failed!");
                 return false;
             }
 
+            $in_file = $file['fqpn'];
+            $work_file = $this::WORKING_DIRECTORY .'/'. $file['filename'];
+
             if (rename($in_file, $work_file) === false) {
                 $queueitem->delete();
-                $mtdla->raiseError(__TRAIT__ ." rename {$in_file} to {$work_file} failed!");
+                $mtdla->raiseError("Rename {$in_file} to {$work_file} failed!");
                 return false;
             }
 
             $json_str = json_encode(
                 array(
-                    'file_name' => $file,
-                    'file_size' => $size,
-                    'file_hash' => $hash,
-                    'state' => 'state'
+                    'file_name' => $file['filename'],
+                    'file_size' => $file['size'],
+                    'file_hash' => $file['hash'],
+                    'state' => 'new'
                 )
             );
 
@@ -165,7 +126,7 @@ class ImportController extends DefaultController
                     $json_str,
                     "import",
                     "queue",
-                    $guid
+                    $file['guid']
                 );
             } catch (Exception $e) {
                 $queueitem->delete();
@@ -180,6 +141,82 @@ class ImportController extends DefaultController
                     return false;
                 }
             }
+        }
+
+        return true;
+    }
+
+    public function scanDirectory($path, &$files)
+    {
+        global $mtlda, $audit;
+
+        if (($importdir = opendir($this::INCOMING_DIRECTORY)) === false) {
+            $mtlda->raiseError("Failed to access ". $this::INCOMING_DIRECTORY);
+            return false;
+        }
+
+        while ($item = readdir($importdir)) {
+
+            // ignore special files and if $file is empty
+            if (empty($item) || $item == '.' || $item == '..') {
+                continue;
+            }
+
+            // ignore file/directories starting with . (aka hidden)
+            if (preg_match("/^\..*/", $item)) {
+                continue;
+            }
+
+            if (is_dir($item)) {
+                if (!$this->scanDirectory($path .'/'. $item, $files)) {
+                    $mtlda->raiseError(__CLASS__ .'::scanDirectory() returned false!');
+                    return false;
+                }
+                continue;
+            }
+
+            $file = array();
+            $file['filename'] = basename($item);
+            $file['fqpn'] = $this::INCOMING_DIRECTORY .'/'. $file['filename'];
+
+
+            if (!file_exists($file['fqpn'])) {
+                $mtdla->raiseError("File {$file['fqpn']} does not exist!");
+                return false;
+            }
+
+            if (!is_file($file['fqpn'])) {
+                $mtlda->raiseError("{$file['fqpn']} is not a file!");
+                return false;
+            }
+
+            if (!is_readable($file['fqpn'])) {
+                $mtdla->raiseError("{$file['fqpn']} is not readable!");
+                return false;
+            }
+
+            try {
+                $audit->log(
+                    "{$file['filename']}",
+                    "new",
+                    "queue"
+                );
+            } catch (\Exception $e) {
+                $mtlda->raiseError("AuditController::log() raised an exception!");
+                return false;
+            }
+
+            if (($file['hash'] = sha1_file($file['fqpn'])) === false) {
+                $mtdla->raiseError("SHA1 value of {$file['fqpn']} can not be calculated!");
+                return false;
+            }
+
+            if (($file['size'] = filesize($file['fqpn'])) === false) {
+                $mtdla->raiseError("Filesize of {$file['fqpn']} is not available!");
+                return false;
+            }
+
+            array_push($files, $file);
         }
 
         return true;
@@ -235,11 +272,11 @@ class ImportController extends DefaultController
                 if (!$this->unlinkDirectory($fqfn)) {
                     return false;
                 }
-            } else {
-                if (!unlink($fqfn)) {
-                    $mtlda->raiseError("unlink() on {$fqfn} returned false!");
-                    return false;
-                }
+            }
+
+            if (!unlink($fqfn)) {
+                $mtlda->raiseError("unlink() on {$fqfn} returned false!");
+                return false;
             }
         }
 
