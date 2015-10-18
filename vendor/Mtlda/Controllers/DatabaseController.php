5263512528086d7f07abf63a2455c3aa76a81958
Andreas Unterkircher <unki@netshadow.at>
2015-10-18 10:20:59 +0200

    rebase our DatabaseController on Thallium DatabaseController, refs #522

---
+++ vendor/Mtlda/Controllers/DatabaseController.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * This file is part of MTLDA.
+ *
+ * MTLDA, a web-based document archive.
+ * Copyright (C) <2015>  <Andreas Unterkircher>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
+
+namespace Mtlda\Controllers;
+
+class DatabaseController extends \Thallium\Controllers\DatabaseController
+{
+    const SCHEMA_VERSION = 21;
+
+    public function truncateDatabaseTables()
+    {
+        global $mtlda;
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXmeta")) === false) {
+            $mtlda->raiseError("failed to truncate 'meta' table!");
+            return false;
+        }
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXjobs")) === false) {
+            $mtlda->raiseError("failed to truncate 'jobs' table!");
+            return false;
+        }
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXmessage_bus")) === false) {
+            $mtlda->raiseError("failed to truncate 'message_bus' table!");
+            return false;
+        }
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXaudit")) === false) {
+            $mtlda->raiseError("failed to truncate 'audit' table!");
+            return false;
+        }
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXqueue")) === false) {
+            $mtlda->raiseError("failed to truncate 'queue' table!");
+            return false;
+        }
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXarchive")) === false) {
+            $mtlda->raiseError("failed to truncate 'archive' table!");
+            return false;
+        }
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXkeywords")) === false) {
+            $mtlda->raiseError("failed to truncate 'keywords' table!");
+            return false;
+        }
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXassign_keywords_to_document")) === false) {
+            $mtlda->raiseError("failed to truncate 'assign_keywords_to_document' table!");
+            return false;
+        }
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXdocument_indices")) === false) {
+            $mtlda->raiseError("failed to truncate 'document_indices' table!");
+            return false;
+        }
+
+        if (($this->query("TRUNCATE TABLE TABLEPREFIXdocument_properties")) === false) {
+            $mtlda->raiseError("failed to truncate 'document_properties' table!");
+            return false;
+        }
+
+        return true;
+    }
+}
+
+// vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
