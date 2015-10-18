20ea5df079990b0d5db04ef325c77b416c930c18
Andreas Unterkircher <unki@netshadow.at>
2015-10-18 10:22:04 +0200

    rebase our ExceptionController on Thallium ExceptionController, refs #522

--- vendor/Mtlda/Controllers/ExceptionController.php
+++ vendor/Mtlda/Controllers/ExceptionController.php
@@ -19,13 +19,8 @@
 
 namespace Mtlda\Controllers;
 
-class ExceptionController extends \Exception
+class ExceptionController extends \Thallium\Controllers\ExceptionController
 {
-    // custom string representation of object
-    public function __toString()
-    {
-        return "Backtrace:<br />\n". str_replace("\n", "<br />\n", parent::getTraceAsString());
-    }
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
