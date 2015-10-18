ab0d7f839bceccc3cf3f6573e32711ba7812f1b4
Andreas Unterkircher <unki@netshadow.at>
2015-10-18 10:09:47 +0200

    rebase our SkeletonView on Thallium SkeletonView, refs #522

--- vendor/Mtlda/Views/SkeletonView.php
+++ vendor/Mtlda/Views/SkeletonView.php
@@ -19,18 +19,8 @@
 
 namespace Mtlda\Views;
 
-class SkeletonView extends DefaultView
+class SkeletonView extends \Thallium\Views\SkeletonView
 {
-    public $class_name = 'skeleton';
-
-    /**
-     * overwrite parent show() method as we do not have a lot
-     * to do here.
-     */
-    public function show()
-    {
-        return $this->fetch('skeleton.tpl');
-    }
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
