65c297e2b19aaa6206720394dd48376e871ba07f
Andreas Unterkircher <unki@netshadow.at>
2015-10-31 18:43:21 +0100

    DatabaseController, remove truncateDatabaseTables() method, now provided by Thallium

--- vendor/Mtlda/Controllers/DatabaseController.php
+++ vendor/Mtlda/Controllers/DatabaseController.php
@@ -21,64 +21,7 @@ namespace Mtlda\Controllers;
 
 class DatabaseController extends \Thallium\Controllers\DatabaseController
 {
-    const SCHEMA_VERSION = 26;
-
-    public function truncateDatabaseTables()
-    {
-        global $mtlda;
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXmeta")) === false) {
-            $this->raiseError("failed to truncate 'meta' table!");
-            return false;
-        }
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXjobs")) === false) {
-            $this->raiseError("failed to truncate 'jobs' table!");
-            return false;
-        }
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXmessage_bus")) === false) {
-            $this->raiseError("failed to truncate 'message_bus' table!");
-            return false;
-        }
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXaudit")) === false) {
-            $this->raiseError("failed to truncate 'audit' table!");
-            return false;
-        }
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXqueue")) === false) {
-            $this->raiseError("failed to truncate 'queue' table!");
-            return false;
-        }
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXarchive")) === false) {
-            $this->raiseError("failed to truncate 'archive' table!");
-            return false;
-        }
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXkeywords")) === false) {
-            $this->raiseError("failed to truncate 'keywords' table!");
-            return false;
-        }
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXassign_keywords_to_document")) === false) {
-            $this->raiseError("failed to truncate 'assign_keywords_to_document' table!");
-            return false;
-        }
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXdocument_indices")) === false) {
-            $this->raiseError("failed to truncate 'document_indices' table!");
-            return false;
-        }
-
-        if (($this->query("TRUNCATE TABLE TABLEPREFIXdocument_properties")) === false) {
-            $this->raiseError("failed to truncate 'document_properties' table!");
-            return false;
-        }
-
-        return true;
-    }
+    const SCHEMA_VERSION = 27;
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
