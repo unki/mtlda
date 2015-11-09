23c06a92068c01b6a943e297756849a040d9654c
Andreas Unterkircher <unki@netshadow.at>
2015-11-09 07:06:52 +0100

    mtlda_rpc.js, delete old rpc_object_delete() function

--- public/resources/mtlda_rpc.js
+++ public/resources/mtlda_rpc.js
@@ -15,49 +15,6 @@
  * GNU Affero General Public License for more details.
  */
 
-function rpc_object_delete(element, del_id)
-{
-    if (del_id == undefined || del_id == '') {
-        alert('invalid "del_id" parameter found!');
-        return;
-    }
-
-    $.ajax({
-        type: 'POST',
-        url: 'rpc.html',
-        data: ({
-            type : 'rpc',
-            action : 'delete',
-            id : del_id
-        }),
-        beforeSend: function () {
-            // change row color to red
-            element.parent().parent().animate({backgroundColor: '#fbc7c7' }, 'fast');
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            alert('Failed to contact server! ' + textStatus);
-        },
-        success: function (data) {
-            if (data == 'ok') {
-                // on flushing, reload the page
-                if (del_id.match(/-flush$/)) {
-                    location.reload();
-                    return;
-                }
-                element.parent().parent().animate({ opacity: 'hide' }, 'fast');
-                return;
-            }
-            // change row color back to white
-            element.parent().parent().animate({backgroundColor: '#ffffff' }, 'fast');
-            alert('Server returned: ' + data + ', length ' + data.length);
-            return;
-        }
-    });
-
-    return true;
-
-} // rpc_object_delete()
-
 function rpc_object_archive(element, obj_id, state)
 {
     if (obj_id == undefined || obj_id == '') {
