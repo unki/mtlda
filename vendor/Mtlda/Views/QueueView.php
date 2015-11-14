39a16013e9e856bff4a1846ddf0796170a67185d
Andreas Unterkircher <unki@netshadow.at>
2015-11-14 10:22:09 +0100

    QueueView, set some caching headers on displaying an queued document

--- vendor/Mtlda/Views/QueueView.php
+++ vendor/Mtlda/Views/QueueView.php
@@ -123,6 +123,8 @@ class QueueView extends DefaultView
         }
 
         header('Content-Type: application/pdf');
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
         header('Content-Length: '. strlen($file['content']));
         print $file['content'];
         return true;
