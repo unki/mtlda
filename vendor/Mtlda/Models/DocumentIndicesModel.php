0a3af327eaa9ec8903f09dd0ed4acdb8ce4e046a
Andreas Unterkircher <unki@netshadow.at>
2015-12-29 10:27:49 +0100

    DocumentIndicesModel, add getIndicies() method

--- vendor/Mtlda/Models/DocumentIndicesModel.php
+++ vendor/Mtlda/Models/DocumentIndicesModel.php
@@ -98,6 +98,15 @@ class DocumentIndicesModel extends DefaultModel
 
         return true;
     }
+
+    public function getIndices()
+    {
+        if (!isset($this->items)) {
+            return false;
+        }
+
+        return $this->items;
+    }
 }
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
