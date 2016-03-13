da0eb42a5ccd5d7e5004f8d535f1a7055f5826d1
Andreas Unterkircher <unki@netshadow.at>
2016-03-13 07:13:18 +0100

    move static.php into Mtlda vendor directory

--- vendor/Mtlda/static.php
+++ vendor/Mtlda/static.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of Thallium.
+ * This file is part of MTLDA.
  *
- * Thallium, a PHP-based framework for web applications.
+ * MTLDA, a web-based document archive.
  * Copyright (C) <2015> <Andreas Unterkircher>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -17,8 +17,6 @@
  * GNU Affero General Public License for more details.
  */
 
-define('APP_BASE', __DIR__);
-
 define('SIGN_TOP_LEFT', 1);
 define('SIGN_TOP_CENTER', 2);
 define('SIGN_TOP_RIGHT', 3);
@@ -28,18 +26,5 @@ define('SIGN_MIDDLE_RIGHT', 6);
 define('SIGN_BOTTOM_LEFT', 7);
 define('SIGN_BOTTOM_CENTER', 8);
 define('SIGN_BOTTOM_RIGHT', 9);
-
-if (!constant('LOG_ERR')) {
-    define('LOG_ERR', 1);
-}
-if (!constant('LOG_WARNING')) {
-    define('LOG_WARNING', 2);
-}
-if (!constant('LOG_INFO')) {
-    define('LOG_INFO', 3);
-}
-if (!constant('LOG_DEBUG')) {
-    define('LOG_DEBUG', 4);
-}
 
 // vim: set filetype=php expandtab softtabstop=4 tabstop=4 shiftwidth=4:
