5e36efbc009091b7fc81326881af3a5202738433
Andreas Unterkircher <unki@netshadow.net>
2016-05-22 09:20:41 +0200

    ArchiveView, no more need to overrule showList(), paging has been included into Thallium

--- vendor/Mtlda/Views/ArchiveView.php
+++ vendor/Mtlda/Views/ArchiveView.php
@@ -23,7 +23,6 @@ class ArchiveView extends DefaultView
 {
     protected static $view_class_name = 'archive';
     public $item_name = 'Document';
-    public $archive;
     protected $item;
     protected $keywords;
     protected $document_properties;
@@ -35,9 +34,14 @@ class ArchiveView extends DefaultView
         global $tmpl;
 
         try {
-            $this->archive = new \Mtlda\Models\ArchiveModel;
+            $archive = new \Mtlda\Models\ArchiveModel;
         } catch (\Exception $e) {
             static::raiseError("Failed to load ArchiveModel!", true);
+            return;
+        }
+
+        if (!$this->setViewData($archive)) {
+            static::raiseError(__CLASS__ .'::setViewData() returned false!', true);
             return;
         }
 
@@ -45,100 +49,6 @@ class ArchiveView extends DefaultView
         parent::__construct();
 
         return;
-    }
-
-    public function showList($pageno = null, $items_limit = null)
-    {
-        global $session;
-
-        if (!isset($pageno) || empty($pageno) || !is_numeric($pageno)) {
-            if ($session->hasVariable(static::$view_class_name .'_current_page')) {
-                if (($current_page = $session->getVariable(static::$view_class_name .'_current_page')) === false) {
-                    static::raiseError(get_class($session) .'::getVariable() returned false!');
-                    return false;
-                }
-            } else {
-                $current_page = 1;
-            }
-        } else {
-            $current_page = $pageno;
-        }
-
-        if (!isset($items_limit) || is_null($items_limit) || !is_numeric($items_limit)) {
-            if ($session->hasVariable(static::$view_class_name .'_current_items_limit')) {
-                if (($current_items_limit = $session->getVariable(
-                    static::$view_class_name .'_current_items_limit'
-                )) === false) {
-                    static::raiseError(get_class($session) .'::getVariable() returned false!');
-                    return false;
-                }
-            } else {
-                $current_items_limit = -1;
-            }
-        } else {
-            $current_items_limit = $items_limit;
-        }
-
-        if (!$this->archive->hasItems()) {
-            return parent::showList();
-        }
-
-        try {
-            $pager = new \Mtlda\Controllers\PagingController(array(
-                'delta' => 2,
-            ));
-        } catch (\Exception $e) {
-            static::raiseError(__METHOD__ .'(), failed to load PagingController!');
-            return false;
-        }
-
-        if (($items = $this->archive->getItems()) === false) {
-            static::raiseError(get_class($this->archive) .'::getItems() returned false!');
-            return false;
-        }
-
-        if (!$pager->setPagingData($items)) {
-            static::raiseError(get_class($pager) .'::setPagingData() returned false!');
-            return false;
-        }
-
-        if (!$pager->setCurrentPage($current_page)) {
-            static::raiseError(get_class($pager) .'::setCurrentPage() returned false!');
-            return false;
-        }
-
-        if (!$pager->setItemsLimit($current_items_limit)) {
-            static::raiseError(get_class($pager) .'::setItemsLimit() returned false!');
-            return false;
-        }
-
-        global $tmpl;
-        $tmpl->assign('pager', $pager);
-
-        if (($data = $pager->getPageData()) === false) {
-            static::raiseError(get_class($pager) .'::getPageData() returned false!');
-            return false;
-        }
-
-        if (!isset($data) || empty($data) || !is_array($data)) {
-            static::raiseError(get_class($pager) .'::getPageData() returned invalid data!');
-            return false;
-        }
-
-        $this->archive_avail_items = array_keys($data);
-        $this->archive_items = $data;
-
-        if (!$session->setVariable(static::$view_class_name .'_current_page', $current_page)) {
-            static::raiseError(get_class($session) .'::setVariable() returned false!');
-            return false;
-        }
-
-        if (!$session->setVariable(static::$view_class_name .'_current_items_limit', $current_items_limit)) {
-            static::raiseError(get_class($session) .'::setVariable() returned false!');
-            return false;
-        }
-
-        return parent::showList();
     }
 
     public function showEdit($id, $guid)
