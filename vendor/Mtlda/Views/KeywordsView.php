0bf19521ecb60e440e7b3a1b9dab5d1e1476ea8c
Andreas Unterkircher <unki@netshadow.net>
2016-05-22 09:20:59 +0200

    KeywordsView, no more need to overrule showList(), paging has been included into Thallium

--- vendor/Mtlda/Views/KeywordsView.php
+++ vendor/Mtlda/Views/KeywordsView.php
@@ -23,7 +23,6 @@ class KeywordsView extends DefaultView
 {
     protected static $view_default_mode = 'list';
     protected static $view_class_name = 'keywords';
-    protected $keywords;
 
     public function __construct()
     {
@@ -32,102 +31,18 @@ class KeywordsView extends DefaultView
         parent::__construct();
 
         try {
-            $this->keywords = new \Mtlda\Models\KeywordsModel;
+            $keywords = new \Mtlda\Models\KeywordsModel;
         } catch (\Exception $e) {
             static::raiseError(__CLASS_ .', failed to load KeywordsModel', true);
             return;
         }
 
+        if (!$this->setViewData($keywords)) {
+            static::raiseError(__CLASS__ .'::setViewData() returned false!', true);
+            return;
+        }
+
         return;
-    }
-
-    public function showList($pageno = null, $items_limit = null)
-    {
-        global $session;
-
-        if (!isset($pageno) || empty($pageno) || !is_numeric($pageno)) {
-            if ($session->hasVariable(static::$view_class_name .'_current_page')) {
-                if (($current_page = $session->getVariable(static::$view_class_name .'_current_page')) === false) {
-                    static::raiseError(get_class($session) .'::getVariable() returned false!');
-                    return false;
-                }
-            } else {
-                $current_page = 1;
-            }
-        } else {
-            $current_page = $pageno;
-        }
-
-        if (!isset($items_limit) || is_null($items_limit) || !is_numeric($items_limit)) {
-            if ($session->hasVariable(static::$view_class_name .'_current_items_limit')) {
-                if (($current_items_limit = $session->getVariable(
-                    static::$view_class_name .'_current_items_limit'
-                )) === false) {
-                    static::raiseError(get_class($session) .'::getVariable() returned false!');
-                    return false;
-                }
-            } else {
-                $current_items_limit = -1;
-            }
-        } else {
-            $current_items_limit = $items_limit;
-        }
-
-        if (!$this->keywords->hasItems()) {
-            return parent::showList();
-        }
-
-        try {
-            $pager = new \Mtlda\Controllers\PagingController(array(
-                'delta' => 2,
-            ));
-        } catch (\Exception $e) {
-            static::raiseError(__METHOD__ .'(), failed to load PagingController!');
-            return false;
-        }
-
-        if (!$pager->setPagingData($this->keywords->getItems())) {
-            static::raiseError(get_class($pager) .'::setPagingData() returned false!');
-            return false;
-        }
-
-        if (!$pager->setCurrentPage($current_page)) {
-            static::raiseError(get_class($pager) .'::setCurrentPage() returned false!');
-            return false;
-        }
-
-        if (!$pager->setItemsLimit($current_items_limit)) {
-            static::raiseError(get_class($pager) .'::setItemsLimit() returned false!');
-            return false;
-        }
-
-        global $tmpl;
-        $tmpl->assign('pager', $pager);
-
-        if (($data = $pager->getPageData()) === false) {
-            static::raiseError(get_class($pager) .'::getPageData() returned false!');
-            return false;
-        }
-
-        if (!isset($data) || empty($data) || !is_array($data)) {
-            static::raiseError(get_class($pager) .'::getPageData() returned invalid data!');
-            return false;
-        }
-
-        $this->avail_items = array_keys($data);
-        $this->items = $data;
-
-        if (!$session->setVariable(static::$view_class_name .'_current_page', $current_page)) {
-            static::raiseError(get_class($session) .'::setVariable() returned false!');
-            return false;
-        }
-
-        if (!$session->setVariable(static::$view_class_name .'_current_items_limit', $current_items_limit)) {
-            static::raiseError(get_class($session) .'::setVariable() returned false!');
-            return false;
-        }
-
-        return parent::showList();
     }
 
     public function keywordsList($params, $content, &$smarty, &$repeat)
