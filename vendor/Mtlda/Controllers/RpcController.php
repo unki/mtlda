0fe08388f31509dd930f67daf758a42011a9d1bf
Andreas Unterkircher <unki@netshadow.net>
2016-11-13 21:24:14 +0100

    RpcController, remove stuff already provided by Thallium now

--- vendor/Mtlda/Controllers/RpcController.php
+++ vendor/Mtlda/Controllers/RpcController.php
@@ -30,112 +30,19 @@ class RpcController extends \Thallium\Controllers\RpcController
 
     public function performApplicationSpecifc()
     {
-        global $mtlda, $router, $query;
+        global $router;
 
-        switch ($query->action) {
-            case 'find-prev-next':
-                $this->rpcFindPrevNextObject();
-                break;
-            case 'get-view':
-                $this->rpcGetViewContent();
-                break;
-            case 'submit-messages':
-                $this->rpcSubmitToMessageBus();
-                break;
-            case 'retrieve-messages':
-                $this->rpcRetrieveFromMessageBus();
-                break;
+        if (!$router->hasQueryParam('action')) {
+            return true;
+        }
+
+        switch ($router->getQueryParam('action')) {
             case 'delete-expired-documents':
-                $this->rpcDeleteExpiredDocuments();
-                break;
-            default:
-                return false;
+                return $this->rpcDeleteExpiredDocuments();
                 break;
         }
 
-        return true;
-    }
-
-    protected function rpcFindPrevNextObject()
-    {
-        global $mtlda, $views;
-
-        $valid_models = array(
-            'queueitem',
-        );
-
-        $valid_directions = array(
-            'next',
-            'prev',
-        );
-
-        if (!isset($_POST['model'])) {
-            static::raiseError('No model requested!');
-            return false;
-        }
-
-        if (!in_array($_POST['model'], $valid_models)) {
-            static::raiseError('unknown model requested: '. htmlentities($_POST['model'], ENT_QUOTES));
-            return false;
-        }
-
-        if (!isset($_POST['id'])) {
-            static::raiseError('id is not set!');
-            return false;
-        }
-
-        $id = $_POST['id'];
-
-        if (!$mtlda->isValidId($id)) {
-            static::raiseError('\$id is invalid');
-            return false;
-        }
-
-        if (!isset($_POST['direction'])) {
-            static::raiseError('direction is not set!');
-            return false;
-        }
-
-        if (!in_array($_POST['direction'], $valid_directions)) {
-            static::raiseError('invalid direction requested: '. htmlentities($_POST['direction'], ENT_QUOTES));
-            return false;
-        }
-
-        if (($id = $mtlda->parseId($id)) === false) {
-            static::raiseError('Unable to parse \$id');
-            return false;
-        }
-
-        switch ($id->model) {
-            case 'queueitem':
-                $model = new \Mtlda\Models\QueueItemModel(array(
-                    'idx' => $id->id,
-                    'guid' => $id->guid
-                ));
-                break;
-        }
-
-        if (!isset($model) || empty($model)) {
-            static::raiseError("Model not found: ". htmlentities($id->modek, ENT_QUOTES));
-            return false;
-        }
-
-        switch ($_POST['direction']) {
-            case 'prev':
-                $prev = $model->prev();
-                if ($prev) {
-                    print "queueitem-". $prev;
-                }
-                break;
-            case 'next':
-                $next = $model->next();
-                if ($next) {
-                    print "queueitem-". $next;
-                }
-                break;
-        }
-
-        return true;
+        return false;
     }
 
     protected function rpcDeleteExpiredDocuments()
@@ -152,76 +59,6 @@ class RpcController extends \Thallium\Controllers\RpcController
             return false;
         }
 
-        return true;
-    }
-
-    protected function rpcGetViewContent()
-    {
-        global $mtlda;
-
-        if (!isset($_POST) || empty($_POST) || !is_array($_POST)) {
-            static::raiseError(__METHOD__ .'(), $_POST is invalid!');
-            return false;
-        }
-
-        if (!isset($_POST['view']) || empty($_POST['view']) || !is_string($_POST['view'])) {
-            static::raiseError(__METHOD__ .'(), $_POST["view"] is invalid!');
-            return false;
-        }
-
-        if (!isset($_POST['data']) || empty($_POST['data']) || !is_array($_POST['data'])) {
-            static::raiseError(__METHOD__ .'(), $_POST["data"] is invalid!');
-            return false;
-        }
-
-        $data = $_POST['data'];
-
-        if (!isset($data['content']) || empty($data['content']) || !is_string($data['content'])) {
-            static::raiseError(__METHOD__ .'(), $data["content"] is invalid!');
-            return false;
-        }
-
-        if (!preg_match('/^[a-z]+$/', strtolower($_POST['view']))) {
-            static::raiseError(__METHOD__ .'(), $_POST["view"] contains invalid data!');
-            return false;
-        }
-
-        if (!preg_match('/^[a-z]+$/', strtolower($data['content']))) {
-            static::raiseError(__METHOD__ .'(), $data["content"] contains invalid data!');
-            return false;
-        }
-
-        if (!$mtlda->loadController("Templates", "tmpl")) {
-            static::raiseError(__METHOD__ .'(), failed to load TemplatesController!');
-            return false;
-        }
-
-        $view_name = ucwords(strtolower($_POST['view']));
-        $view_name = "\\Mtlda\\Views\\${view_name}View";
-
-        try {
-            $view = new $view_name;
-        } catch (\Exception $e) {
-            static::raiseError(__METHOD__ ."(), failed to load view ${view_name}!");
-            return false;
-        }
-
-        if (!$view->hasContent($data['content'])) {
-            static::raiseError(get_class($view) .'::hasContent() returned false!');
-            return false;
-        }
-
-        if (($content = $view->getContent($data['content'], $data)) === false) {
-            static::raiseError(get_class($view) .'::getContent() returned false!');
-            return false;
-        }
-
-        if (!isset($content) || empty($content)) {
-            static::raiseError(get_class($view) .'::getContent() returned invalid content!');
-            return false;
-        }
-
-        print $content;
         return true;
     }
 }
