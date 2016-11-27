a84869d5d25e411f3c00b5b8120dcc3a68f5fd29
Andreas Unterkircher <unki@netshadow.net>
2016-11-27 08:23:45 +0100

    main.php, update

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
  * This file is part of Thallium.
  *
  * Thallium, a PHP-based framework for web applications.
- * Copyright (C) <2015> <Andreas Unterkircher>
+ * Copyright (C) <2015-2016> <Andreas Unterkircher>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -17,6 +17,24 @@
  * GNU Affero General Public License for more details.
  */
 
+/**
+ * This file is the starting point for an application.
+ *
+ * It registers the autoloader and initializes the applications MainController -
+ * which then takes over control.
+ *
+ * Usually this script is kicked by public/index.php. index.php is the only
+ * script that needs to be accessible via the web server. Everything else that
+ * belongs to Thallium, or any application that is based on it, should be out
+ * of the document root and not accessible via the web server.
+ *
+ * @license AGPL3
+ * @copyright 2015-2016 Andreas Unterkircher <unki@netshadow.net>
+ * @author Andreas Unterkircher <unki@netshadow.net>
+ */
+
+declare(strict_types=1);
+
 require_once 'vendor/Thallium/static.php';
 require_once 'vendor/Mtlda/static.php';
 require_once 'vendor/autoload.php';
@@ -26,8 +44,10 @@ spl_autoload_register("autoload");
 $mode = null;
 
 if (isset($_SERVER) &&
-    isset($_SERVER['argv']) &&
+    array_key_exists('argv', $_SERVER) &&
+    array_key_exists(1, $_SERVER['argv']) &&
     isset($_SERVER['argv'][1]) &&
+    !empty($_SERVER['argv'][1]) &&
     $_SERVER['argv'][1] == 'incoming'
 ) {
     $mode = 'queue_only';
